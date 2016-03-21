e18caaa1a7ab31f7e4c87280844d4a996b5c1968
kyrielrvingcoding <kyrie.lrving.coding@gmail.comkyrie.lrving.coding@gmail.comkyrie.lrving.coding@gmail.comkyrie.lrving.coding@gmail.comkyrie.lrving.coding@gmail.comkyrie.lrving.coding@gmail.comkyrie.lrving.coding@gmail.comkyrie.lrving.coding@gmail.comkyrie.lrving.coding@gmail.com123456>
2016-03-21 22:50:49 +0800

    dddd

---
+++ dasd/dasd/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  dasd
+//
+//  Created by 诸超杰 on 16/3/21.
+//  Copyright © 2016年 诸超杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ dasd/dasd/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  dasd
+//
+//  Created by 诸超杰 on 16/3/21.
+//  Copyright © 2016年 诸超杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
